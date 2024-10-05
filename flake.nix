01168910f665e70c0764b754cdd517dd134ced10
blockdoth <pepijn.pve@gmail.com>
2024-10-05 20:53:23 +0200

    nixified repo

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Sorting algorithm showcase";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          shellHook = "
+            echo 'Entering a python shell template'
+          ";
+          packages = with pkgs.python3Packages; [
+            python
+            matplotlib
+            numpy
+            imageio
+          ];
+        };
+      });
+    };
+}
